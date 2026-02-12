b7476b5b868eb3cde503caa8853f9cd7ea395754
Andreas Schwob <andreas.schwob@gmail.com>
2026-02-12 10:32:36 +0100

    added viewport rule for responsivity

--- index.js
+++ index.js
@@ -1,58 +1,56 @@
 let homeScore = 0;
 let guestScore = 0;
-let homeScoreBoard = document.getElementById("home-score-board")
-let guestScoreBoard = document.getElementById("guest-score-board")
+let homeScoreBoard = document.getElementById("home-score-board");
+let guestScoreBoard = document.getElementById("guest-score-board");
 
 function leader() {
-  if(homeScore > guestScore) {
-    homeScoreBoard.style.color = "green"
-    guestScoreBoard.style.color = "#F94F6D"
-  }
-  else if (homeScore < guestScore) {
-    homeScoreBoard.style.color = "#F94F6D"
-    guestScoreBoard.style.color = "green"
-  }
-  else {
-    homeScoreBoard.style.color = "white"
-    guestScoreBoard.style.color = "white"
+  if (homeScore > guestScore) {
+    homeScoreBoard.style.color = "green";
+    guestScoreBoard.style.color = "#F94F6D";
+  } else if (homeScore < guestScore) {
+    homeScoreBoard.style.color = "#F94F6D";
+    guestScoreBoard.style.color = "green";
+  } else {
+    homeScoreBoard.style.color = "white";
+    guestScoreBoard.style.color = "white";
   }
 }
 
 function homescore1() {
   homeScore += 1;
   homeScoreBoard.innerText = homeScore;
-  leader()
+  leader();
 }
 function homescore2() {
   homeScore += 2;
   homeScoreBoard.innerText = homeScore;
-  leader()
+  leader();
 }
 function homescore3() {
   homeScore += 3;
   homeScoreBoard.innerText = homeScore;
-  leader()
+  leader();
 }
 function guestscore1() {
   guestScore += 1;
   guestScoreBoard.innerText = guestScore;
-  leader()
+  leader();
 }
 function guestscore2() {
   guestScore += 2;
   guestScoreBoard.innerText = guestScore;
-  leader()
+  leader();
 }
 function guestscore3() {
   guestScore += 3;
   guestScoreBoard.innerText = guestScore;
-  leader()
+  leader();
 }
 
 function newgame() {
-  homeScore = 0
-  guestScore = 0
+  homeScore = 0;
+  guestScore = 0;
   homeScoreBoard.innerText = homeScore;
   guestScoreBoard.innerText = guestScore;
-  leader()
+  leader();
 }
